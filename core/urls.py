139206b9b71fbe2ba0500c463dee5defe413bda3
sixdgt <c4crypt@gmail.com>
2025-06-20 14:08:26 +0545

    Django Project LMS: assignment views create + list & templates index + form

--- core/urls.py
+++ core/urls.py
@@ -10,7 +10,9 @@ from core.views import (
    StudentListView,
    StudentDetailView,
    StudentDeleteView,
-   StudentUpdateView
+   StudentUpdateView,
+   AssignmentListView,
+   AssignmentCreateView,
 )
 
 urlpatterns = [
@@ -29,5 +31,8 @@ urlpatterns = [
    path('student/create/', StudentCreateView.as_view(), name='student.create'),
    path('student/<int:pk>/detail/', StudentDetailView.as_view(), name='student.detail'),
    path('student/<int:pk>/edit/', StudentUpdateView.as_view(), name='student.edit'),
-   path('student/<int:pk>/delete/', StudentDeleteView.as_view(), name='student.delete')
+   path('student/<int:pk>/delete/', StudentDeleteView.as_view(), name='student.delete'),
+   # Assignment URLs
+   path('assignment/list/', AssignmentListView.as_view(), name='assignment.index'),
+   path('assignment/create/', AssignmentCreateView.as_view(), name='assignment.create'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,16 +1,19 @@
+from re import L
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from core.models import Teacher, Student, Assignment, Material
 from django.urls import reverse_lazy
 from core.forms import TeacherForm
+from django.contrib.auth.mixins import LoginRequiredMixin # this package is for class based views
+# from django.contrib.auth.decorators import login_required - this package is for function based views
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
 # Teacher Views
-class TeacherListView(ListView):
+class TeacherListView(LoginRequiredMixin, ListView):
     model = Teacher
     template_name = 'teachers/teacher_index.html'
     context_object_name = 'teachers'
@@ -21,55 +24,68 @@ class TeacherListView(ListView):
         context['teachers'] = context['page_obj']  # Now 'teachers' is the Page object
         return context
 
-class TeacherCreateView(CreateView):
+class TeacherCreateView(LoginRequiredMixin, CreateView):
     model = Teacher
     form_class = TeacherForm
     template_name = 'teachers/teacher_form.html'
     success_url = reverse_lazy('teacher.index')
 
-class TeacherDetailView(DetailView):
+class TeacherDetailView(LoginRequiredMixin, DetailView):
     model = Teacher
     template_name = 'teachers/teacher_detail.html'
     context_object_name = 'teacher'
 
-class TeacherUpdateView(UpdateView):
+class TeacherUpdateView(LoginRequiredMixin, UpdateView):
     model = Teacher
     form_class = TeacherForm
     template_name = 'teachers/teacher_form.html'
     success_url = reverse_lazy('teacher.index')
 
-class TeacherDeleteView(DeleteView):
+class TeacherDeleteView(LoginRequiredMixin, DeleteView):
     model = Teacher
     context_object_name = 'teacher'
     template_name = 'teachers/teacher_delete_confirm.html'
     success_url = reverse_lazy('teacher.index')
 
 # Student Views
-class StudentListView(ListView):
+class StudentListView(LoginRequiredMixin, ListView):
     model = Student
     template_name = 'students/student_index.html'
     context_object_name = 'students'
     paginate_by = 5
 
-class StudentCreateView(CreateView):
+class StudentCreateView(LoginRequiredMixin, CreateView):
     model = Student
     fields = ['full_name', 'email', 'semester', 'phone_no', 'user']
     template_name = 'students/student_form.html'
     success_url = reverse_lazy('student.index')
 
-class StudentDetailView(DetailView):
+class StudentDetailView(LoginRequiredMixin, DetailView):
     model = Student
     template_name = 'students/student_detail.html'
     context_object_name = 'student'
 
-class StudentUpdateView(UpdateView):
+class StudentUpdateView(LoginRequiredMixin, UpdateView):
     model = Student
     template_name = 'students/student_form.html'
     fields = ['full_name', 'email', 'semester', 'phone_no', 'user']
     success_url = reverse_lazy('student.index')
 
-class StudentDeleteView(DeleteView):
+class StudentDeleteView(LoginRequiredMixin, DeleteView):
     model = Student
     context_object_name = 'student'
     template_name = 'students/student_delete_confirm.html'
     success_url = reverse_lazy('student.index')
+
+# Assignment Views
+class AssignmentListView(LoginRequiredMixin, ListView):
+    model = Assignment
+    template_name = 'assignments/assignment_index.html'
+    context_object_name = 'assignments'
+    paginate_by = 5
+
+class AssignmentCreateView(LoginRequiredMixin, CreateView):
+    model = Assignment
+    fields = ['title', 'start_date', 'end_date', 'full_mark', 'assignment_subject']
+    template_name = 'assignments/assignment_form.html'
+    success_url = reverse_lazy('assignment.index')
